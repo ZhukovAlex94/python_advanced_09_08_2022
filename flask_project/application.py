805e8e0f9330848e56f2aff444c4d80d510558b1
nik <python.hillel@gmail.com>
2022-09-02 21:36:22 +0300

    New Flask project

---
+++ flask_project/application.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+from flask import Flask
+from flask import render_template
+
+from werkzeug.exceptions import abort
+
+app = Flask(__name__)
+
+
+def get_connection():
+    conn = sqlite3.connect('books_db.sqlite')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+@app.route('/books/list')
+def list_books():
+    conn = get_connection()
+    books = conn.execute('SELECT * FROM books;').fetchall()
+    conn.close()
+
+    return render_template('books/list.html', books=books)
+
+
+@app.route('/books/detail/<int:book_id>')
+def detail_book(book_id):
+    conn = get_connection()
+    book = conn.execute('SELECT * FROM books WHERE id = ?', (book_id,)).fetchone()
+    conn.close()
+    if book is None:
+        abort(404)
+
+    return render_template('books/detail.html', book=book)
+
+
+app.run(debug=True, port=5050)
